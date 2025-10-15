19968d0118a70588411367d823829c36f8615822
Mahim <mahim807034@gmail.com>
2025-10-15 16:37:12 +0600

    task-11

---
+++ task-11.js
@@ -0,0 +1,14 @@
+function isPerfectNumber(num) {
+  if (num <= 1) return false; 
+
+  let sum = 0;
+
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) {
+      sum += i;
+    }
+  }
+
+  return sum === num;
+}
+console.log(isPerfectNumber(20));
