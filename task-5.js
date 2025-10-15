d9e16f8f94f9e232db47cc099a379a0263576bac
Mahim <mahim807034@gmail.com>
2025-10-15 16:17:28 +0600

    task-5

---
+++ task-5.js
@@ -0,0 +1,11 @@
+function reverseDigits(num) {
+  let str = num.toString(); 
+  let reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];  
+  }
+
+  return parseInt(reversedStr);
+}
+console.log(reverseDigits(965));
