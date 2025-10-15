3ba978a79b6300e150a06be48cf9928a40cff345
Mahim <mahim807034@gmail.com>
2025-10-15 16:47:47 +0600

    task-13

---
+++ task-13.js
@@ -0,0 +1,10 @@
+function generateRandomOdd() {
+  let num;
+
+  do {
+    num = Math.floor(Math.random() * 50) + 1;
+  } while (num % 2 === 0); 
+
+  return num;
+}
+console.log(generateRandomOdd());
